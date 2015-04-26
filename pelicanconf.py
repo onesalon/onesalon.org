aa9816479e22d368f7ba0c0c01ccc9fdbf168c7e
Kurt Spindler <kurt@uber.com>
2015-04-25 20:12:07 -0700

    update settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = False
 
