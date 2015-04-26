b5707c7c5dcf58022ef4f3534a91dd255e211c59
Kurt Spindler <kurt@uber.com>
2015-04-25 19:26:42 -0700

    fix

---
+++ One-Salon-Values.md
@@ -0,0 +1,11 @@
+# Introduction
+
+# Values
+
+## 1. Community
+
+## 2. Creativity
+
+## 3. Expression
+
+## 4. Learning
--- fabfile.py
+++ fabfile.py
@@ -9,14 +9,9 @@ import SocketServer
 env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
 
-# Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
 
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
+def bootstrap():
+    local('pip install pelican markdown')
 
 
 def clean():
@@ -24,15 +19,19 @@ def clean():
         local('rm -rf {deploy_path}'.format(**env))
         local('mkdir {deploy_path}'.format(**env))
 
+
 def build():
     local('pelican -s pelicanconf.py')
+
 
 def rebuild():
     clean()
     build()
 
+
 def regenerate():
     local('pelican -r -s pelicanconf.py')
+
 
 def serve():
     os.chdir(env.deploy_path)
@@ -46,28 +45,25 @@ def serve():
     sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
     server.serve_forever()
 
+
 def reserve():
     build()
     serve()
 
+
 def preview():
     local('pelican -s publishconf.py')
 
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
 
-@hosts(production)
 def publish():
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True,
-        extra_opts='-c',
-    )
+    build()
+    local('git checkout gh-pages')
+    if not env.deploy_path:
+        return
+    local('rm -f /tmp/{deploy_path} && mv {deploy_path} /tmp/'.format(**env))
+    local('rm -rf .')
+    local('mv /tmp/{deploy_path}/* ./'.format(**env))
+    local('git add .')
+    local('git commit -m $(date)')
+    local('git push')
+    local('git checkout master')
